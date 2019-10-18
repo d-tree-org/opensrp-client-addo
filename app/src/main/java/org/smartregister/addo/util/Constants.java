ac909bd3f5a2e37c9a8fbe5ecc6ab4f9b2faa6f7
Isaiah <issyzac.iz@gmail.com>
2019-10-18 16:43:24 +0300

    iM | feat: included core app util files

---
+++ app/src/main/java/org/smartregister/addo/util/Constants.java
@@ -0,0 +1,33 @@
+package org.smartregister.addo.util;
+
+public class Constants {
+
+    public static String EC_CLIENT_FIELDS = "ec_client_fields.json";
+
+    public static class CONFIGURATION {
+        public static final String LOGIN = "login";
+        public static final String FAMILY_REGISTER = "family_register";
+        public static final String FAMILY_MEMBER_REGISTER = "family_member_register";
+        public static final String ANC_REGISTER = "anc_register";
+        public static final String MALARIA_REGISTER = "anc_malaria_confirmation";
+
+    }
+
+    public static final class DrawerMenu {
+        public static final String ALL_FAMILIES = "All Families";
+        public static final String ANC_CLIENTS = "ANC Clients";
+        public static final String CHILD_CLIENTS = "Child Clients";
+        public static final String HIV_CLIENTS = "Hiv Clients";
+        public static final String ANC = "ANC";
+        public static final String LD = "L&D";
+        public static final String PNC = "PNC";
+        public static final String FAMILY_PLANNING = "Family Planning";
+        public static final String MALARIA = "Malaria";
+    }
+
+    public static final class PEER_TO_PEER {
+
+        public static final String LOCATION_ID = "location-id";
+    }
+
+}
---
+++ app/src/main/java/org/smartregister/addo/util/Utils.java
@@ -0,0 +1,14 @@
+package org.smartregister.addo.util;
+
+import org.smartregister.CoreLibrary;
+
+/**
+ * Author : Isaya Mollel on 2019-10-18.
+ */
+public class Utils {
+
+    public static org.smartregister.Context context() {
+        return CoreLibrary.getInstance().context();
+    }
+
+}
