6368f255f5dc3c8d5e8d253bda55012eac3ba979
Isaiah <issyzac.iz@gmail.com>
2019-10-18 16:47:39 +0300

    iM | feat: added implementations for the login interactor

--- app/src/main/java/org/smartregister/addo/interactor/LoginInteractor.java
+++ app/src/main/java/org/smartregister/addo/interactor/LoginInteractor.java
@@ -1,10 +1,27 @@
 package org.smartregister.addo.interactor;
 
+import org.smartregister.addo.BuildConfig;
+import org.smartregister.job.SyncServiceJob;
+import org.smartregister.login.interactor.BaseLoginInteractor;
+import org.smartregister.view.contract.BaseLoginContract;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Author : Isaya Mollel on 2019-10-18.
  */
-public class LoginInteractor {
+public class LoginInteractor extends BaseLoginInteractor implements BaseLoginContract.Interactor {
 
+    public LoginInteractor(BaseLoginContract.Presenter loginPresenter){super(loginPresenter);}
 
+    @Override
+    protected void scheduleJobsPeriodically() {
+//        SyncServiceJob.scheduleJob(SyncServiceJob.TAG, TimeUnit.MINUTES.toMinutes(BuildConfig.DATA_SYNC_DURATION_MINUTES), getFlexValue(BuildConfig
+//                .DATA_SYNC_DURATION_MINUTES));
+    }
 
+    @Override
+    protected void scheduleJobsImmediately() {
+        //super.scheduleJobsImmediately();
+    }
 }
