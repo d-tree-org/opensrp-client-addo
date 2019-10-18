df38345cd46b24dbc44dcf90a7b5bd22352f1278
Isaiah <issyzac.iz@gmail.com>
2019-10-18 16:46:59 +0300

    iM | feat: added implementations for login presenter

--- app/src/main/java/org/smartregister/addo/presenter/LoginPresenter.java
+++ app/src/main/java/org/smartregister/addo/presenter/LoginPresenter.java
@@ -1,7 +1,83 @@
 package org.smartregister.addo.presenter;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.smartregister.addo.R;
+import org.smartregister.addo.application.AddoApplication;
+import org.smartregister.addo.interactor.LoginInteractor;
+import org.smartregister.configurableviews.model.LoginConfiguration;
+import org.smartregister.configurableviews.model.ViewConfiguration;
+import org.smartregister.configurableviews.util.Constants;
+import org.smartregister.login.model.BaseLoginModel;
+import org.smartregister.login.presenter.BaseLoginPresenter;
+import org.smartregister.view.contract.BaseLoginContract;
+
+import java.lang.ref.WeakReference;
+
 /**
  * Author : Isaya Mollel on 2019-10-18.
  */
-public class LoginPresenter {
+public class LoginPresenter extends BaseLoginPresenter implements BaseLoginContract.Presenter {
+
+    public final static String TAG = BaseLoginPresenter.class.getCanonicalName();
+
+    public LoginPresenter(BaseLoginContract.View loginView){
+        mLoginView = new WeakReference<>(loginView);
+        mLoginInteractor  = new LoginInteractor(this);
+        mLoginModel = new BaseLoginModel();
+    }
+
+    @Override
+    public void processViewCustomizations() {
+        try {
+            String jsonString = getJsonViewFromPreference(Constants.VIEW_CONFIGURATION_PREFIX + Constants.CONFIGURATION.LOGIN);
+            if (jsonString == null) {
+                return;
+            }
+
+            ViewConfiguration loginView = AddoApplication.getJsonSpecHelper().getConfigurableView(jsonString);
+            LoginConfiguration metadata = (LoginConfiguration) loginView.getMetadata();
+            LoginConfiguration.Background background = metadata.getBackground();
+
+            CheckBox showPasswordCheckBox = getLoginView().getActivityContext().findViewById(R.id.login_show_password_checkbox);
+            TextView showPasswordTextView = getLoginView().getActivityContext().findViewById(R.id.login_show_password_text_view);
+            if (!metadata.getShowPasswordCheckbox()) {
+                showPasswordCheckBox.setVisibility(View.GONE);
+                showPasswordTextView.setVisibility(View.GONE);
+            } else {
+                showPasswordCheckBox.setVisibility(View.VISIBLE);
+                showPasswordTextView.setVisibility(View.VISIBLE);
+            }
+
+            if (background.getOrientation() != null && background.getStartColor() != null && background.getEndColor() != null) {
+                View loginLayout = getLoginView().getActivityContext().findViewById(R.id.login_layout);
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.setShape(GradientDrawable.RECTANGLE);
+                gradientDrawable.setOrientation(
+                        GradientDrawable.Orientation.valueOf(background.getOrientation()));
+                gradientDrawable.setColors(new int[]{Color.parseColor(background.getStartColor()),
+                        Color.parseColor(background.getEndColor())});
+                loginLayout.setBackground(gradientDrawable);
+            }
+
+            ImageView imageView = getLoginView().getActivityContext().findViewById(R.id.login_logo);
+            if (metadata.getLogoUrl() != null) {
+
+            }
+
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean isServerSettingsSet() {
+        return false;
+    }
 }
