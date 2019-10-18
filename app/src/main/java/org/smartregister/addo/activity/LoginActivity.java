7341a89b8425c47d17ed49250a6e63d57c6ed73f
Isaiah <issyzac.iz@gmail.com>
2019-10-18 16:51:24 +0300

    iM | feat: User login functionality

--- app/src/main/java/org/smartregister/addo/activity/LoginActivity.java
+++ app/src/main/java/org/smartregister/addo/activity/LoginActivity.java
@@ -1,7 +1,54 @@
 package org.smartregister.addo.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.smartregister.addo.R;
+import org.smartregister.addo.presenter.LoginPresenter;
+import org.smartregister.view.activity.BaseLoginActivity;
+import org.smartregister.view.contract.BaseLoginContract;
+
 /**
  * Author : Isaya Mollel on 2019-10-18.
  */
-public class LoginActivity {
+public class LoginActivity extends BaseLoginActivity implements BaseLoginContract.View {
+
+    public static final String TAG = BaseLoginActivity.class.getCanonicalName();
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initializePresenter() {
+        mLoginPresenter = new LoginPresenter(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void goToHome(boolean b) {
+        //Take user to a home page
+        if (b){
+            goToMainActivity();
+        }
+    }
+
+    private void goToMainActivity(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLoginPresenter.processViewCustomizations();
+        if (mLoginPresenter.isUserLoggedOut()){
+            goToHome(false);
+        }
+    }
 }
