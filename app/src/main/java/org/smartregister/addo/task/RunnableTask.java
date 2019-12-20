89947368f972247ae2e26e6f3ff8711b35a34ca7
gosso22 <sheghembekassim@gmail.com>
2019-12-20 18:14:49 +0300

    Chore | Include the CoreConstants from the chw core.

--- app/src/main/java/org/smartregister/addo/task/RunnableTask.java
+++ app/src/main/java/org/smartregister/addo/task/RunnableTask.java
@@ -1,4 +1,22 @@
 package org.smartregister.addo.task;
 
-public class RunnableTask {
+import android.os.AsyncTask;
+
+/**
+ * Runs and runnable code async
+ */
+public class RunnableTask extends AsyncTask<Void, Void, Void> {
+    private Runnable runnable;
+
+    public RunnableTask(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        if(runnable != null){
+            runnable.run();
+        }
+        return null;
+    }
 }
