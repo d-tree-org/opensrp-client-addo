3d9e4c33a65a1b32beb8ca494cdc2123d54ae99b
issyzac <issyzac.iz@gmail.com>
2020-12-10 15:03:44 +0300

    Helper class for the dashboard

---
+++ app/src/main/java/org/smartregister/addo/util/ChartUtil.java
@@ -0,0 +1,15 @@
+package org.smartregister.addo.util;
+
+public class ChartUtil {
+
+    /// Current Month Indicators
+    public static String adolescentEncounter = "IND_CURR_ADOLESCENT";
+    public static String ancEncounter = "IND_CURR_ANC";
+    public static String pncEncounter = "IND_CURR_PNC";
+    public static String childEncounter = "IND_CURR_CHILD";
+    public static String otherEncounter = "IND_CURR_OTHER";
+
+    public static String lastMonthTotalEncounters = "IND_LAST_TOTAL";
+    public static String lastMonthReferredEncounters = "IND_LAST_REFERRED";
+
+}
