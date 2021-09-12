4b40c553d7c1b8c12b2ced8ded8b9c193b2853b4
issyzac <issyzac.iz@gmail.com>
2021-09-12 21:55:22 +0300

    Added report indicators

--- app/src/main/java/org/smartregister/addo/util/ChartUtil.java
+++ app/src/main/java/org/smartregister/addo/util/ChartUtil.java
@@ -3,13 +3,30 @@ package org.smartregister.addo.util;
 public class ChartUtil {
 
     /// Current Month Indicators
-    public static String adolescentEncounter = "IND_CURR_ADOLESCENT";
-    public static String ancEncounter = "IND_CURR_ANC";
-    public static String pncEncounter = "IND_CURR_PNC";
-    public static String childEncounter = "IND_CURR_CHILD";
-    public static String otherEncounter = "IND_CURR_OTHER";
+    public static final String adolescentEncounter = "IND_CURR_ADOLESCENT";
+    public static final String ancEncounter = "IND_CURR_ANC";
+    public static final String pncEncounter = "IND_CURR_PNC";
+    public static final String childEncounter = "IND_CURR_CHILD";
+    public static final String otherEncounter = "IND_CURR_OTHER";
 
-    public static String lastMonthTotalEncounters = "IND_LAST_TOTAL";
-    public static String lastMonthReferredEncounters = "IND_LAST_REFERRED";
+    public static final String currentMonthLinked = "IND_CURR_MONTH_LINKED";
+    public static final String currentMonthLinkedAttended = "IND_CURR_MONTH_LINKED_ATTENDED";
+
+    public static final String currentMonthIssuedByAddo = "IND_CURR_MONTH_ISSUED";
+    public static final String currentMonthIssuedAttendedByAddo = "IND_CURR_MONTH_ISSUED";
+
+    public static final String lastMonthIssuedByAddo = "IND_LAST_MONTH_ISSUED";
+    public static final String lastMonthIssuedAttendedByAddo = "IND_CURR_MONTH_ISSUED";
+
+    public static final String currentMonthVisits = "IND_CURR_MONTH_VISITS";
+
+    public static final String lastMonthTotalEncounters = "IND_LAST_TOTAL";
+    public static final String lastMonthReferredEncounters = "IND_LAST_REFERRED";
+    public static final String lastMonthLinked = "IND_LAST_MONTH_LINKED";
+    public static final String lastMonthAttendedLinked = "IND_LAST_MONTH_LINKED_ATTENDED";
+
+    public static final String percentageAttended = "IND_PERCENTAGE_ATTENDED";
+
+    public static final String lastMonthVisits = "IND_LAST_MONTH_VISITS";
 
 }
