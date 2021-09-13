ccea7bf1235fa4831e34548bee044e4e9c3bd064
issyzac <issyzac.iz@gmail.com>
2021-09-13 09:23:37 +0300

    new report indicator code definition

--- app/src/main/java/org/smartregister/addo/util/ChartUtil.java
+++ app/src/main/java/org/smartregister/addo/util/ChartUtil.java
@@ -13,10 +13,12 @@ public class ChartUtil {
     public static final String currentMonthLinkedAttended = "IND_CURR_MONTH_LINKED_ATTENDED";
 
     public static final String currentMonthIssuedByAddo = "IND_CURR_MONTH_ISSUED";
-    public static final String currentMonthIssuedAttendedByAddo = "IND_CURR_MONTH_ISSUED";
+    public static final String currentMonthIssuedAttendedByAddo = "IND_CURR_MONTH_ISSUED_ATTENDED";
+    public static final String currentMonthCompleted = "IND_CURR_MONTH_COMPLETED";
 
     public static final String lastMonthIssuedByAddo = "IND_LAST_MONTH_ISSUED";
-    public static final String lastMonthIssuedAttendedByAddo = "IND_CURR_MONTH_ISSUED";
+    public static final String lastMonthIssuedAttendedByAddo = "IND_LAST_MONTH_ISSUED_ATTENDED";
+    public static final String lastMonthCompleted = "IND_LAST_MONTH_COMPLETED";
 
     public static final String currentMonthVisits = "IND_CURR_MONTH_VISITS";
 
@@ -26,6 +28,8 @@ public class ChartUtil {
     public static final String lastMonthAttendedLinked = "IND_LAST_MONTH_LINKED_ATTENDED";
 
     public static final String percentageAttended = "IND_PERCENTAGE_ATTENDED";
+    public static final String percentageCompleted = "IND_PERCENTAGE_COMPLETED";
+    public static final String percentageCompletedLastMonth = "IND_PERCENTAGE_COMPLETED_LM";
 
     public static final String lastMonthVisits = "IND_LAST_MONTH_VISITS";
 
