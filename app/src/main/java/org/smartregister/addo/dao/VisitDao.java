0a41cced4764f5b4ab8ef9c978f2eec87e94c165
Allan O <a.onchuru@gmail.com>
2021-10-22 17:06:37 +0300

    :recycle: Read latest visit record & return Visit object

--- app/src/main/java/org/smartregister/addo/dao/VisitDao.java
+++ app/src/main/java/org/smartregister/addo/dao/VisitDao.java
@@ -1,9 +1,9 @@
 package org.smartregister.addo.dao;
 
-import org.smartregister.chw.anc.AncLibrary;
 import org.smartregister.chw.anc.domain.Visit;
 import org.smartregister.dao.AbstractDao;
 
+import java.util.Date;
 import java.util.List;
 
 public class VisitDao extends AbstractDao {
@@ -17,15 +17,24 @@ public class VisitDao extends AbstractDao {
                 "and visit_details.details = '" + encounterType + "'\n" +
                 "order by visits.visit_date DESC limit 1;";
 
-        DataMap<List<Visit>> dataMap = cursor -> {
-            List<Visit> visits= AncLibrary.getInstance().visitRepository().readVisits(cursor);
-            if (cursor != null)
-                cursor.close();
+        DataMap<Visit> dataMap = cursor -> {
+            Visit visit = new Visit();
+            visit.setVisitId(getCursorValue(cursor, "visit_id"));
+            visit.setVisitType(getCursorValue(cursor, "visit_type"));
+            visit.setParentVisitID(getCursorValue(cursor, "parent_visit_id"));
+            visit.setPreProcessedJson(getCursorValue(cursor, "pre_processed"));
+            visit.setBaseEntityId(getCursorValue(cursor, "base_entity_id"));
+            visit.setDate(new Date(Long.parseLong(getCursorValue(cursor, "visit_date"))));
+            visit.setJson(getCursorValue(cursor, "visit_json"));
+            visit.setFormSubmissionId(getCursorValue(cursor, "form_submission_id"));
+            visit.setProcessed(getCursorIntValue(cursor, "processed") == 1);
+            visit.setCreatedAt(new Date(Long.parseLong(getCursorValue(cursor, "created_at"))));
+            visit.setUpdatedAt(new Date(Long.parseLong(getCursorValue(cursor, "updated_at"))));
 
-            return visits;
+            return visit;
         };
 
-        List<Visit> visits = readData(sql, dataMap).get(0);
+        List<Visit> visits = readData(sql, dataMap);
         if (visits == null || visits.size() != 1)
             return null;
 
